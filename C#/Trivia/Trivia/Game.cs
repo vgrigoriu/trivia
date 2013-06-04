b76d5dd56ac60b3f45fc054339d8cc1b50842a7a
Victor Grigoriu <vgrigoriu@gmail.com>
2013-06-04 13:13:54 +0300

    Run several games for better coverage

--- C#/Trivia/Trivia/Game.cs
+++ C#/Trivia/Trivia/Game.cs
@@ -33,7 +33,7 @@ namespace UglyTrivia
 
             for (int i = 0; i < 50; i++)
             {
-                popQuestions.AddLast("Poop Question " + i);
+                popQuestions.AddLast("Pop Question " + i);
                 scienceQuestions.AddLast(("Science Question " + i));
                 sportsQuestions.AddLast(("Sports Question " + i));
                 rockQuestions.AddLast(createRockQuestion(i));
